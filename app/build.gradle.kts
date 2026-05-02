356460a4065db196da838554e441a1f504639178
palaziks <gochmyradov0@gmail.com>
2026-05-02 19:43:15 +0500

    Add dependencies for Hilt, Compose, and OkHttp

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+// Hilt
+implementation("com.google.dagger:hilt-android:2.51")
+kapt("com.google.dagger:hilt-compiler:2.51")
+implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+// Compose + Nav
+implementation("androidx.navigation:navigation-compose:2.7.7")
+implementation("androidx.compose.material3:material3:1.2.1")
+implementation("androidx.compose.material:material-icons-extended:1.6.7")
+
+// OkHttp (subscription fetching + ping)
+implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+// Lifecycle / ViewModel
+implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
